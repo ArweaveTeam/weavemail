11e20744403acdf1617c8a7ebca60f130b5ac476
codi45 <codi4572@gmail.com>
2019-04-19 07:54:50 -0400

    Add previous Message in reply

--- view.js
+++ view.js
@@ -5,16 +5,32 @@ function show_mail(txid) {
 		var mail_id = document.getElementById("view_title");
 		var view_contents = document.getElementById("view_contents");
 		var reply = document.getElementById("reply");
+		var previousSender= document.getElementById('previousSender');
+		var previousMsg= document.getElementById('previousMsg');
+		var previousDateMsg= document.getElementById('previousDateMsg');
+		var unixTime="0";
 
 		mail_id.textContent = txid;
 
 		var tx = await arweave.transactions.get(txid);
+
+		tx.get('tags').forEach(tag => {
+		let key = tag.get('name', {decode: true, string: true});
+		let value = tag.get('value', {decode: true, string: true});
+		if(key==="UnixTime") unixTime=value;
+
+		});
 
 		var key = await wallet_to_key(wallet);
 
 		reply.onclick = async function() {
 			var recipient = document.getElementById("compose_address");
 			recipient.value = await arweave.wallets.ownerToAddress(tx.owner);
+
+			previousSender.innerHTML=recipient.value;
+			previousDateMsg.innerHTML= timeConverter(unixTime);
+			previousMsg.innerHTML=mail;
+
 			switch_to_page("compose_page");
 		};
 
@@ -25,5 +41,20 @@ function show_mail(txid) {
 		mail = mail.replace(/(?:\r\n|\r|\n)/g, '<br>');
 
 		view_contents.innerHTML = mail;
+
+
+		function timeConverter(UNIX_timestamp){
+  	var a = new Date(UNIX_timestamp * 1000);
+  	var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  	var year = a.getFullYear();
+  	var month = months[a.getMonth()];
+  	var date = a.getDate();
+  	var hour = a.getHours();
+  	var min = a.getMinutes();
+  	var sec = a.getSeconds();
+  	var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  	return time;
+		}
+		
 	})();
 }
