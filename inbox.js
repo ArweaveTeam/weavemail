f0c9a0fc68ed3a2dad291d48523233a72331f110
Lev Berman <ldmberman@protonmail.com>
2019-04-21 15:19:01 +0200

    Use Promise.all to await for all requests
    The motivation is clarity. Now the process is explicit: "await all" -> "reset page" -> "sort and display new".

--- inbox.js
+++ inbox.js
@@ -20,14 +20,12 @@ function refresh_inbox(wallet) {
 			};
 
     	const res = await this.arweave.api.post(`arql`, query);
-		var mail_UnixTime = [];
+		var tx_rows = [];
 		if(res.data == "") {
-			var mail_ids = [];
+			tx_rows = [];
 		}
 		else {
-			var mail_ids = res.data;
-
-			mail_ids.map(async function(id,i) {
+		    tx_rows = await Promise.all(res.data.map(async function(id,i) {
 
 				let tx_row={};
 				var tx = await this.arweave.transactions.get(id);
@@ -45,56 +43,45 @@ function refresh_inbox(wallet) {
 			tx_row['td_fee'] = arweave.ar.winstonToAr(tx.reward);
 			tx_row['td_qty'] = arweave.ar.winstonToAr(tx.quantity);
 
-			mail_UnixTime.push(tx_row);
-
-			if(mail_UnixTime.length===mail_ids.length)	displayMails();
-
-		});
+			return tx_row;
+		}));
 
 		}
-
-		console.log(mail_ids);
 
 		var mail_address = document.getElementById("mail_address");
 		mail_address.innerHTML = address;
 
 		var inbox_pane = document.getElementById("inbox_pane");
-
 		while (inbox_pane.firstChild) {
 			inbox_pane.removeChild(inbox_pane.firstChild);
 		}
 
+		tx_rows.sort((a,b)=>(Number(b.unixTime) - Number(a.unixTime)));
 
-			function displayMails(){
+        inbox_pane.innerHTML="";
+        tx_rows.forEach( function(item) {
+            var tr = document.createElement("tr");
+            var td_name = document.createElement("td");
+            var td_from = document.createElement("td");
+            var td_fee = document.createElement("td");
+            var td_qty = document.createElement("td");
+            var link = document.createElement("a");
+            tr.appendChild(td_from);
+            tr.appendChild(td_name);
+            tr.appendChild(td_fee);
+            tr.appendChild(td_qty);
+            td_name.appendChild(link);
 
-			mail_UnixTime.sort((a,b)=>(Number(b.unixTime) - Number(a.unixTime)));
+            var tx_status = item.tx_status;
+            td_from.innerHTML = item.from;
+            td_fee.innerHTML = item.td_fee;
+            td_qty.innerHTML = item.td_qty;
 
+            link.innerHTML = item.id;
+            link.onclick = function() { show_mail(item.id) };
 
-			inbox_pane.innerHTML="";
-			mail_UnixTime.forEach( function(item) {
-				var tr = document.createElement("tr");
-				var td_name = document.createElement("td");
-				var td_from = document.createElement("td");
-				var td_fee = document.createElement("td");
-				var td_qty = document.createElement("td");
-				var link = document.createElement("a");
-				tr.appendChild(td_from);
-				tr.appendChild(td_name);
-				tr.appendChild(td_fee);
-				tr.appendChild(td_qty);
-				td_name.appendChild(link);
-
-				var tx_status = item.tx_status;
-				td_from.innerHTML = item.from;
-				td_fee.innerHTML = item.td_fee;
-				td_qty.innerHTML = item.td_qty;
-
-				link.innerHTML = item.id;
-				link.onclick = function() { show_mail(item.id) };
-
-				inbox_pane.appendChild(tr);
-			});
-			}
+            inbox_pane.appendChild(tr);
+        });
 
 	})();
 }
