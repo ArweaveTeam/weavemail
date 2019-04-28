d3b112976f438f7ccf422b417d978f3f62ebabcc
codi45 <codi4572@gmail.com>
2019-04-28 07:29:45 -0400

    Add subject Line

--- compose.js
+++ compose.js
@@ -6,6 +6,8 @@ function send_mail () {
     (async () => {
         var compose_address = document.getElementById('compose_address')
         var address = compose_address.value
+        var compose_subject = document.getElementById('compose_subject')
+        var subject = compose_subject.value
         var compose_content = document.getElementById('compose_content')
         var content = compose_content.value
         var compose_tokens = document.getElementById('compose_tokens')
@@ -24,7 +26,7 @@ function send_mail () {
             return
         }
 
-        content = await encrypt_mail(content, pub_key)
+        content = await encrypt_mail(content,subject, pub_key)
         console.log(content)
 
         var tx =
