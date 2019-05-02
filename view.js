8955bb491d3f8b1b2c214946c541a70ea9d09666
codi45 <codi4572@gmail.com>
2019-05-02 13:18:07 -0400

    Add subject Line

--- view.js
+++ view.js
@@ -4,6 +4,7 @@ function show_mail (txid) {
 
         var mail_id = document.getElementById('view_title')
         var view_contents = document.getElementById('view_contents')
+        var view_subject = document.getElementById('view_subject')
         var reply = document.getElementById('reply')
         var previousSender = document.getElementById('previousSender')
         var previousMsg = document.getElementById('previousMsg')
@@ -13,8 +14,8 @@ function show_mail (txid) {
         var previous_page = document.getElementById('previous_page')
         var unixTime = '0'
 
-        mail_id.textContent = txid
-
+      //  mail_id.textContent = txid
+        view_subject.innerHTML=txid
         var tx = await arweave.transactions.get(txid)
 
         tx.get('tags').forEach(tag => {
@@ -32,11 +33,11 @@ function show_mail (txid) {
             previousDateMsg.innerHTML = timeConverter(unixTime)
             previousSender.innerHTML = recipient.value
             previousMsg.innerHTML = mail
-            previousSubject.innerHTML=""
+            previousSubject.innerHTML=txid
 
             if(mailContent){
               previousMsg.innerHTML = mailContent.body
-              previousSubject.innerHTML=mailContent.subject
+            //  previousSubject.innerHTML=txid;
             }
 
             (unixTime === '0') ? previousDateTag.style.display = 'none' : previousDateTag.style.display = 'block'
@@ -57,12 +58,12 @@ function show_mail (txid) {
         } catch (e) {}
 
         view_contents.innerHTML = mail
-        view_subject.innerHTML = ""
+      //  view_subject.innerHTML = ""
 
 
         if(mailContent){
         view_contents.innerHTML = mailContent.body
-        view_subject.innerHTML = mailContent.subject
+      //  view_subject.innerHTML = mailContent.subject
         }
 
         function timeConverter (UNIX_timestamp) {
