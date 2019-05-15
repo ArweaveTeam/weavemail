983e6e0072505863aa37793138b02f2b9b3a0b8d
Sam Williams <samuel.ecwilliams@gmail.com>
2019-05-15 09:30:10 -0400

    Added support for `?to=[addr]` addresses.

--- login.js
+++ login.js
@@ -4,8 +4,17 @@ function login (files) {
         try {
             wallet = JSON.parse(ev.target.result)
 
-            refresh_inbox(wallet)
-            switch_to_page('inbox_page')
+            var params = new URLSearchParams(window.location.search);
+
+            if(!params.has('to')) {
+                refresh_inbox(wallet)
+                switch_to_page('inbox_page')
+            }
+            else {
+                var compose_address = document.getElementById('compose_address')
+                compose_address.value = params.get('to')
+                compose_mail()
+            }
 
             setInterval(function () { refresh_inbox(wallet) }, 30000)
         } catch (err) {
